f91a9404e41fdc852cb4505c5ff3c9ae9499e0b0
dan <dan@Lakehouse-MacBook-Pro.local>
2014-04-05 12:58:00 -0400

    added test class

---
+++ GDGTest.h
@@ -0,0 +1,13 @@
+//
+//  GDGTest.h
+//  Git Test
+//
+//  Created by dan on 4/5/14.
+//  Copyright (c) 2014 GDGA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GDGTest : NSObject
+
+@end
---
+++ GDGTest.m
@@ -0,0 +1,13 @@
+//
+//  GDGTest.m
+//  Git Test
+//
+//  Created by dan on 4/5/14.
+//  Copyright (c) 2014 GDGA. All rights reserved.
+//
+
+#import "GDGTest.h"
+
+@implementation GDGTest
+
+@end
